b429aa4c10c4a7060632788a3344597ea2913f95
duong dono <duong.dno95@gmail.com>
2022-10-21 21:53:21 +1300

    21/10/22 commit header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,6 +17,16 @@ function Header() {
                     <div className="search__divider"></div>
                     <i class="fa-solid fa-magnifying-glass"></i>
                 </div>
+                <div className="header__items">
+                    <div className="header__item1">
+                        <i class="fa-solid fa-plus"></i>
+                        <p className="upload__btn">Upload</p>
+                    </div>
+                    <button className="header__item2" type="button">
+                        Login
+                    </button>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div>
             </div>
         </header>
     );
